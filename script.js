014cc603912ddf95146dda15aba939ef87d497b0
ryanascherr <ryanascherr@gmail.com>
2021-03-19 10:08:02 -0400

    Add functionality to save value of time block to local storage.

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
+var hourNineTextContent = document.getElementById("hour-nine-text-area");
 //display date/time: done
-//make time blocks for hours 9-5:
+//make time blocks for hours 9-5: done
 //
-
-var timeBlock = document.querySelectorAll(".time-block");
-
-var currentTime = moment().format("H");
-var currentTimeNumber = parseInt(currentTime);
-
-var numberString = [];
-var actualNumber = [];
-    for (i = 0; i < timeBlock.length; i++) {
-        numberString[i] = timeBlock[i].getAttribute("data-time");
-        actualNumber[i] = parseInt(numberString[i]);
-        if (currentTimeNumber > actualNumber[i]) {
-            timeBlock[i].classList.add("past");
-        } else if (currentTimeNumber === actualNumber[i]) {
-            timeBlock[i].classList.add("present");
-        } else {
-            timeBlock[i].classList.add("future");
-        }
-    }
-    
 function displayDate() {
     document.getElementById("date").textContent = moment().format("MMMM Do YYYY, h:mm:ss a");
     setInterval(function() {
         document.getElementById("date").textContent = moment().format("MMMM Do YYYY, h:mm:ss a");
 }, 1000);
 }
-displayDate();
 
-// function checkTime() {
-//     var currentTime = moment().format("H");
-//     var currentTimeNumber = parseInt(currentTime);
-    
-//     var nine = document.getElementById("hour-9").getAttribute("data-time");
-//     console.log(nine);
-//     var nineNumber = parseInt(nine);
+function styleTimeBlocks() {
 
-//     var test = [];
-//     for (i = 0; i < timeBlock.length; i++) {
-//         timeBlock[i].getAttribute("data-time");
-//         test[i] = parseInt(timeBlock[i]);
-//     }
+var timeBlock = document.querySelectorAll(".time-block");
+var currentTime = moment().format("H");
+var currentTimeNumber = parseInt(currentTime);
+var numberString = [];
+var actualNumber = [];
 
-//     if (currentTimeNumber > nineNumber) {
-//         hourNine.setAttribute("class", "row time-block border past");
-//     } else if (currentTimeNumber < nineNumber) {
-//         hourNine.setAttribute("class", "row time-block border future");
-//     } else {
-//         hourNine.setAttribute("class", "row time-block border present");
-//     }
-// }
+for (i = 0; i < timeBlock.length; i++) {
+    numberString[i] = timeBlock[i].getAttribute("data-time");
+    actualNumber[i] = parseInt(numberString[i]);
+    if (currentTimeNumber > actualNumber[i]) {
+        timeBlock[i].classList.add("past");
+    } else if (currentTimeNumber === actualNumber[i]) {
+        timeBlock[i].classList.add("present");
+    } else {
+        timeBlock[i].classList.add("future");
+    }
+}
+}
 
-// checkTime();
+function initialize() {
+    displayDate();
+    styleTimeBlocks();
+    var hourNineTextNew = localStorage.getItem("hour-nine-timeblock-text");
+    document.getElementById("hour-nine-text-area").value = hourNineTextNew;
+}
+
+initialize ();
+
+document.getElementById("hour-nine-button").addEventListener("click", function(){
+    localStorage.setItem("hour-nine-timeblock-text", document.getElementById("hour-nine-text-area").value);
+})
